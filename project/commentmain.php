ed58040dbb050017bbed5fb000953efed0822408
shanu0809 <54330504+shanu0809@users.noreply.github.com>
2020-06-23 22:12:44 +0530

    query1

---
+++ project/commentmain.php
@@ -0,0 +1,24 @@
+<!DOCTYYPE html>
+<html>
+  <head>
+    <title>
+      query
+    </title>
+    <script src="js/comment.js"></script>
+    <link  rel="stylesheet" href="css/comment.css" >
+  </head>
+  <body><div style="width: 80%;height:50px;border:10px;padding-left:40px;">
+    <h1 align="center">Ask Your Queries</h1>
+
+
+    <!-- GIVE YOUR PAGE OR PRODUCT A POST ID -->
+    <input type="hidden" id="post_id" value="999"/>
+
+    <!-- CREATE A CONTAINER TO LOAD COMMENTS -->
+    <div id="comments"></div>
+
+    <!-- CREATE A CONTAINER TO LOAD REPLY DOCKET -->
+    <div id="reply-main"></div>
+  </div>
+  </body>
+</html>
---
+++ project/js/googlemap.js
@@ -0,0 +1,78 @@
+var map;
+var geocoder;
+
+function loadMap() {
+	var india = {lat:20.5937, lng:78.9629 };
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: india
+    });
+
+    var marker = new google.maps.Marker({
+      position: india,
+      map: map
+    });
+
+    var cdata = JSON.parse(document.getElementById('data').innerHTML);
+    geocoder = new google.maps.Geocoder();  
+    codeAddress(cdata);
+
+    var allData = JSON.parse(document.getElementById('allData').innerHTML);
+    showAllColleges(allData)
+}
+
+function showAllColleges(allData) {
+	var infoWind = new google.maps.InfoWindow;
+	Array.prototype.forEach.call(allData, function(data){
+		var content = document.createElement('div');
+		var strong = document.createElement('strong');
+		
+		strong.textContent = data.name;
+		content.appendChild(strong);
+
+		var img = document.createElement('img');
+		img.src = 'img/Leopard.jpg';
+		img.style.width = '100px';
+		content.appendChild(img);
+
+		var marker = new google.maps.Marker({
+	      position: new google.maps.LatLng(data.lat, data.lng),
+	      map: map
+	    });
+
+	    marker.addListener('mouseover', function(){
+	    	infoWind.setContent(content);
+	    	infoWind.open(map, marker);
+	    })
+	})
+}
+
+function codeAddress(cdata) {
+   Array.prototype.forEach.call(cdata, function(data){
+    	var address = data.name + ' ' + data.address + ' ' + data.country + ' ' + data.state + ' ' + data.city;
+	    geocoder.geocode( { 'address': address ,'country': country , 'state': state , 'city' : city}, function(results, status) {
+	      if (status == 'OK') {
+	        map.setCenter(results[0].geometry.location);
+	        var points = {};
+	        points.id = data.id;
+	        points.lat = map.getCenter().lat();
+	        points.lng = map.getCenter().lng();
+	        updateCollegeWithLatLng(points);
+	      } else {
+	        alert('Geocode was not successful for the following reason: ' + status);
+	      }
+	    });
+	});
+}
+
+function updateCollegeWithLatLng(points) {
+	$.ajax({
+		url:"action.php",
+		method:"post",
+		data: points,
+		success: function(res) {
+			console.log(res)
+		}
+	})
+	
+}
